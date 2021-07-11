d9cab9db4fc77c1eaed450e51e21cbfa6318b9bf
NasterQ <83660024+NasterQ@users.noreply.github.com>
2021-07-11 18:38:02 +0100

    5th exercise for 12.07.21

---
+++ 12.07.21/Rooster game/Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.t0 = New System.Windows.Forms.Label()
+        Me.turnLab = New System.Windows.Forms.Label()
+        Me.t1 = New System.Windows.Forms.Label()
+        Me.t2 = New System.Windows.Forms.Label()
+        Me.t3 = New System.Windows.Forms.Label()
+        Me.t4 = New System.Windows.Forms.Label()
+        Me.t5 = New System.Windows.Forms.Label()
+        Me.t6 = New System.Windows.Forms.Label()
+        Me.t7 = New System.Windows.Forms.Label()
+        Me.t8 = New System.Windows.Forms.Label()
+        Me.resBut = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label1.Location = New System.Drawing.Point(90, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(10, 207)
+        Me.Label1.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label3.Location = New System.Drawing.Point(180, 23)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(10, 207)
+        Me.Label3.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label2.Location = New System.Drawing.Point(12, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(259, 12)
+        Me.Label2.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label4.Location = New System.Drawing.Point(12, 159)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(259, 12)
+        Me.Label4.TabIndex = 4
+        '
+        't0
+        '
+        Me.t0.BackColor = System.Drawing.Color.Transparent
+        Me.t0.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.t0.Location = New System.Drawing.Point(20, 20)
+        Me.t0.Margin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.t0.Name = "t0"
+        Me.t0.Size = New System.Drawing.Size(70, 55)
+        Me.t0.TabIndex = 5
+        '
+        'turnLab
+        '
+        Me.turnLab.AutoSize = True
+        Me.turnLab.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.turnLab.Location = New System.Drawing.Point(9, 244)
+        Me.turnLab.Name = "turnLab"
+        Me.turnLab.Size = New System.Drawing.Size(110, 32)
+        Me.turnLab.TabIndex = 8
+        Me.turnLab.Text = "Turn: O"
+        '
+        't1
+        '
+        Me.t1.BackColor = System.Drawing.Color.Transparent
+        Me.t1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.t1.Location = New System.Drawing.Point(110, 20)
+        Me.t1.Margin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.t1.Name = "t1"
+        Me.t1.Size = New System.Drawing.Size(70, 55)
+        Me.t1.TabIndex = 17
+        '
+        't2
+        '
+        Me.t2.BackColor = System.Drawing.Color.Transparent
+        Me.t2.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.t2.Location = New System.Drawing.Point(199, 20)
+        Me.t2.Margin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.t2.Name = "t2"
+        Me.t2.Size = New System.Drawing.Size(70, 55)
+        Me.t2.TabIndex = 18
+        '
+        't3
+        '
+        Me.t3.BackColor = System.Drawing.Color.Transparent
+        Me.t3.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.t3.Location = New System.Drawing.Point(20, 100)
+        Me.t3.Margin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.t3.Name = "t3"
+        Me.t3.Size = New System.Drawing.Size(70, 55)
+        Me.t3.TabIndex = 19
+        '
+        't4
+        '
+        Me.t4.BackColor = System.Drawing.Color.Transparent
+        Me.t4.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.t4.Location = New System.Drawing.Point(110, 100)
+        Me.t4.Margin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.t4.Name = "t4"
+        Me.t4.Size = New System.Drawing.Size(70, 55)
+        Me.t4.TabIndex = 20
+        '
+        't5
+        '
+        Me.t5.BackColor = System.Drawing.Color.Transparent
+        Me.t5.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.t5.Location = New System.Drawing.Point(199, 100)
+        Me.t5.Margin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.t5.Name = "t5"
+        Me.t5.Size = New System.Drawing.Size(70, 55)
+        Me.t5.TabIndex = 21
+        '
+        't6
+        '
+        Me.t6.BackColor = System.Drawing.Color.Transparent
+        Me.t6.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.t6.Location = New System.Drawing.Point(20, 180)
+        Me.t6.Margin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.t6.Name = "t6"
+        Me.t6.Size = New System.Drawing.Size(70, 55)
+        Me.t6.TabIndex = 22
+        '
+        't7
+        '
+        Me.t7.BackColor = System.Drawing.Color.Transparent
+        Me.t7.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.t7.Location = New System.Drawing.Point(110, 180)
+        Me.t7.Margin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.t7.Name = "t7"
+        Me.t7.Size = New System.Drawing.Size(70, 55)
+        Me.t7.TabIndex = 23
+        '
+        't8
+        '
+        Me.t8.BackColor = System.Drawing.Color.Transparent
+        Me.t8.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.t8.Location = New System.Drawing.Point(199, 180)
+        Me.t8.Margin = New System.Windows.Forms.Padding(3, 0, 0, 0)
+        Me.t8.Name = "t8"
+        Me.t8.Size = New System.Drawing.Size(70, 55)
+        Me.t8.TabIndex = 24
+        '
+        'resBut
+        '
+        Me.resBut.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.resBut.Location = New System.Drawing.Point(249, 244)
+        Me.resBut.Name = "resBut"
+        Me.resBut.Size = New System.Drawing.Size(124, 46)
+        Me.resBut.TabIndex = 25
+        Me.resBut.Text = "Restart"
+        Me.resBut.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(395, 318)
+        Me.Controls.Add(Me.resBut)
+        Me.Controls.Add(Me.t8)
+        Me.Controls.Add(Me.t7)
+        Me.Controls.Add(Me.t6)
+        Me.Controls.Add(Me.t5)
+        Me.Controls.Add(Me.t4)
+        Me.Controls.Add(Me.t3)
+        Me.Controls.Add(Me.t2)
+        Me.Controls.Add(Me.t1)
+        Me.Controls.Add(Me.turnLab)
+        Me.Controls.Add(Me.t0)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Rooster Game"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents t0 As Label
+    Friend WithEvents turnLab As Label
+    Friend WithEvents t1 As Label
+    Friend WithEvents t2 As Label
+    Friend WithEvents t3 As Label
+    Friend WithEvents t4 As Label
+    Friend WithEvents t5 As Label
+    Friend WithEvents t6 As Label
+    Friend WithEvents t7 As Label
+    Friend WithEvents t8 As Label
+    Friend WithEvents resBut As Button
+End Class
---
+++ 12.07.21/Rooster game/Form1.vb
@@ -0,0 +1,140 @@
+﻿Public Class Form1
+
+    Dim sizeG = 9
+    Dim turn = 0
+    Dim board As New List(Of Integer)
+    Dim board_e As New List(Of Label)
+    Dim control = True
+    Dim turns = 0
+
+    Function ShowMessage(ByVal ch As Integer)
+        If ch = 0 Then
+            MessageBox.Show("O wins!")
+        Else
+            MessageBox.Show("X wins!")
+        End If
+        control = False
+        turns = 0
+    End Function
+
+    Function Highlight(ByVal tl1 As Integer, ByVal tl2 As Integer, ByVal tl3 As Integer)
+        board_e(tl1).ForeColor = Color.LawnGreen
+        board_e(tl2).ForeColor = Color.LawnGreen
+        board_e(tl3).ForeColor = Color.LawnGreen
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialize an array: negative numbers mean: there is nothing on the tile
+        For i = 0 To sizeG
+            board.Add(-(i + 1))
+        Next
+
+        board_e.Add(t0)
+        board_e.Add(t1)
+        board_e.Add(t2)
+        board_e.Add(t3)
+        board_e.Add(t4)
+        board_e.Add(t5)
+        board_e.Add(t6)
+        board_e.Add(t7)
+        board_e.Add(t8)
+
+    End Sub
+
+
+    ' Game functionality
+    Private Sub Char_Click(sender As Object, e As EventArgs) Handles t4.Click, t1.Click, t3.Click, t2.Click, t0.Click, t8.Click, t7.Click, t6.Click, t5.Click
+
+        ' Calculate which field was clicked
+        Dim offset = 15 ' for Y offset: +1
+        Dim l = sender.Location
+
+        ' Calculate the field's index
+        ' For Y: *3 for X: +1
+        Dim xx = (l.X - offset) \ 67
+        Dim yy = ((l.Y - offset - 1) \ 65) * 3
+        Dim index = xx + yy
+        If control = False Then
+            Return
+        End If
+
+        If turn = 0 And board(index) <> 0 And board(index) <> 1 Then
+            sender.Text = "O"
+            turnLab.Text = "Turn: X"
+            turn = 1
+            board(index) = 0
+            turns += 1
+        ElseIf board(index) <> 0 And board(index) <> 1 Then
+            sender.Text = "X"
+            turn = 0
+            turnLab.Text = "Turn: O"
+            board(index) = 1
+            turns += 1
+        End If
+
+        ' Check if won
+
+        If board(0) = board(1) And board(1) = board(2) Then
+            Highlight(0, 1, 2)
+            ShowMessage(board(0))
+        End If
+
+        If board(3) = board(4) And board(4) = board(5) Then
+            Highlight(3, 4, 5)
+            ShowMessage(board(3))
+        End If
+
+        If board(6) = board(7) And board(7) = board(8) Then
+            Highlight(6, 7, 8)
+            ShowMessage(board(6))
+        End If
+
+        If board(0) = board(3) And board(3) = board(6) Then
+            Highlight(0, 3, 6)
+            ShowMessage(board(0))
+        End If
+
+        If board(1) = board(4) And board(4) = board(7) Then
+            Highlight(1, 4, 7)
+            ShowMessage(board(1))
+        End If
+
+        If board(2) = board(5) And board(5) = board(8) Then
+            Highlight(2, 5, 8)
+            ShowMessage(board(2))
+        End If
+
+        If board(0) = board(4) And board(4) = board(8) Then
+            Highlight(0, 4, 8)
+            ShowMessage(board(0))
+        End If
+
+        If board(2) = board(4) And board(4) = board(6) Then
+            Highlight(2, 4, 6)
+            ShowMessage(board(2))
+        End If
+
+
+        ' Check if draw (all turns have been done and nobody won)
+        If turns = 9 And control = True Then
+            MessageBox.Show("Draw! Nobody wins!")
+            control = False
+            turns = 0
+        End If
+
+    End Sub
+
+    Private Sub resBut_Click(sender As Object, e As EventArgs) Handles resBut.Click
+        For i = 0 To sizeG
+            board(i) = (-(i + 1))
+        Next
+
+        For i = 0 To 8
+            board_e(i).ForeColor = Color.Black
+            board_e(i).Text = ""
+        Next
+
+        control = True
+
+    End Sub
+End Class
